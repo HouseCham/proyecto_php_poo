c1714ed42d282e46ac5b1890177e60e33cfec72c
Ramses Rmz <a00344520@tec.mx>
2022-05-13 17:04:38 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de camisetas</title>
+</head>
+<body>
+    <!-- header -->
+    <header id="header">
+        <div id="logo">
+            <img src="assets/imgs/logo.png" alt="t-shirt"/>
+            <a href="index.php">
+                Tienda de camisetas
+            </a>
+        </div>
+    </header>
+    <!-- Menu -->
+    <!-- Nabvar -->
+    <!-- Main -->
+    <!-- Footer -->
+</body>
+</html>
