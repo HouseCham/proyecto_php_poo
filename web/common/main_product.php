bdc663affdec5b22dba29f7d98fad3ed49ea196d
Ramses Rmz <a00344520@tec.mx>
2022-05-18 00:19:05 -0500

    Products css done

--- web/common/main_product.php
+++ web/common/main_product.php
@@ -1,6 +1,22 @@
-<div class="product">
-    <img src="assets/imgs/logo.png" alt="" class="product_img">
-    <h2>Blue T-Shirt</h2>
-    <p>$500</p>
-    <a href="#">Buy</a>
+<div id="central">
+    <h1>Productos destacados</h1>
+    
+    <div class="product">
+        <img src="assets/imgs/logo.png" alt="" class="product_img">
+        <h2>Blue T-Shirt</h2>
+        <p>$500</p>
+        <a href="#" class="button">Buy</a>
+    </div>
+    <div class="product">
+        <img src="assets/imgs/logo.png" alt="" class="product_img">
+        <h2>Blue T-Shirt</h2>
+        <p>$500</p>
+        <a href="#" class="button">Buy</a>
+    </div>
+    <div class="product">
+        <img src="assets/imgs/logo.png" alt="" class="product_img">
+        <h2>Blue T-Shirt</h2>
+        <p>$500</p>
+        <a href="#" class="button">Buy</a>
+    </div>
 </div>
