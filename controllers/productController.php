8e976316e0d7d58fafd3a538328d8716ace4ea76
Ramses Rmz <a00344520@tec.mx>
2022-05-23 14:28:11 -0500

    add product done

--- controllers/productController.php
+++ controllers/productController.php
@@ -13,4 +13,17 @@ class productController{
         $products = $product->getAll();
         require_once 'views/product/manage.php';
     }
+
+    public function create(){
+        Utils::isAdmin();
+
+        require_once 'views/product/create.php';
+    }
+
+    public function save(){
+        Utils::isAdmin();
+        if(isset($_POST)){
+            var_dump($_POST);
+        }
+    }
 }
